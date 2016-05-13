8d4442ca658204d1f4d32506746db8f0f94bc0ab
Dmitriy <Dimones951@yandex.ru>
2016-05-13 18:48:12 +0600

    Update MD5 func

--- MyNSTU/API/MNAPI+Addition.m
+++ MyNSTU/API/MNAPI+Addition.m
@@ -264,15 +264,25 @@
 }
 - (NSString*) getMD5
 {
-    const char* str = [self UTF8String];
+//    const char* str = [self UTF8String];
+//    unsigned char result[CC_MD5_DIGEST_LENGTH];
+//    CC_MD5(str, sizeof(str), result);
+//    
+//    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH*2];
+//    for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
+//        [ret appendFormat:@"%02x",result[i]];
+//    }
+//    return ret;
+    const char *cStr = [self UTF8String];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(str, sizeof(str), result);
-    
-    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH*2];
-    for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
-        [ret appendFormat:@"%02x",result[i]];
-    }
-    return ret;
+    CC_MD5( cStr, (int)strlen(cStr), result ); // This is the md5 call
+    return [NSString stringWithFormat:
+            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+            result[0], result[1], result[2], result[3],
+            result[4], result[5], result[6], result[7],
+            result[8], result[9], result[10], result[11],
+            result[12], result[13], result[14], result[15]
+            ];
 }
 - (NSString*) encodeBase64
 {
